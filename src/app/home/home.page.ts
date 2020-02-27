ebeab81b481d9a9aa8a04fac489bbecded9be231
ArthurBrunet <43806925+ArthurBrunet@users.noreply.github.com>
2020-02-27 15:50:25 +0100

    Step Compter

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,8 @@ import {Gyroscope, GyroscopeOrientation, GyroscopeOptions} from '@ionic-native/g
 import {HttpClient, HttpHeaders, HttpErrorResponse} from '@angular/common/http';
 
 import {Data} from '../Models/Data';
+import {Result} from '../Models/Result';
+import {element} from 'protractor';
 
 
 const apiUrl = 'http://185.216.25.16:5000/datas';
@@ -20,16 +22,32 @@ const apiUrl = 'http://185.216.25.16:5000/datas';
 export class HomePage {
     public Array = [];
     public pseudo: string;
+    public step = 0;
     public Data: Data;
+
+    public result: Result;
+
+    public maxX: number;
+    public minX: number;
+
+    public maxY: number;
+    public minY: number;
+
+    public maxZ: number;
+    public minZ: number;
+
     public x = 0;
     public y = 0;
     public z = 0;
+
     public accX = 0;
     public accY = 0;
     public accZ = 0;
-    public positionX: number;
-    public positionY: number;
-    public positionZ: number;
+
+    public positionX = 0;
+    public positionY = 0;
+    public positionZ = 0;
+
     private timestamp: any;
 
 
@@ -41,9 +59,12 @@ export class HomePage {
 
 
     constructor(private deviceMotion: DeviceMotion, private gyroscope: Gyroscope, private api: HttpClient) {
-        this.positionX = 0;
-        this.positionY = 0;
-        this.positionZ = 0;
+        this.minX = 0;
+        this.maxX = 0;
+        this.minY = 0;
+        this.maxY = 0;
+        this.minZ = 0;
+        this.maxZ = 0;
     }
 
 
@@ -73,6 +94,24 @@ export class HomePage {
 
         setInterval(() => {
             this.position(this.accX, this.accY, this.accZ);
+            if (this.minX > this.accX) {
+                this.minX = this.accX;
+            }
+            if (this.maxX < this.accX) {
+                this.maxX = this.accX;
+            }
+            if (this.minY > this.accY) {
+                this.minY = this.accY;
+            }
+            if (this.maxY < this.accY) {
+                this.maxY = this.accY;
+            }
+            if (this.minZ > this.accZ) {
+                this.minZ = this.accZ;
+            }
+            if (this.maxZ < this.accZ) {
+                this.maxZ = this.accZ;
+            }
             this.Data = {
                 x: this.x,
                 y: this.y,
@@ -86,12 +125,66 @@ export class HomePage {
                 timestamp: this.timestamp,
                 pseudo: this.pseudo
             };
+
             this.Array.push(this.Data);
         }, 10);
 
         setInterval(() => {
+            const x = (this.maxX - this.minX);
+            const y = (this.maxY - this.minY);
+            const z = (this.maxZ - this.minZ);
+            this.result = {
+                X: x,
+                Y: y,
+                Z: z,
+            };
+            const AxeMax = Math.max(this.result.X, this.result.Y, this.result.Z);
+            if ( this.result.X == AxeMax) {
+                let treshold = ((this.minX + this.maxX) / 2);
+                for (let i = 0; i < this.Array.length; i++){
+                    let a = i + 1;
+                    if (i !== (this.Array.length - 1) && (treshold > 0.5 || treshold < -0.5)) {
+                        if (this.Array[i]["accX"] >= treshold && this.Array[a]["accX"] <= treshold) {
+                            this.step++;
+                        }
+                    }
+
+                }
+            }
+            if ( this.result.Y == AxeMax ) {
+                let treshold = ((this.minY + this.maxY) / 2);
+                for (let i = 0; i < this.Array.length; i++){
+                    let a = i + 1;
+                    if (i !== (this.Array.length - 1) && (treshold > 0.5 || treshold < -0.5)) {
+                        if (this.Array[i]["accY"] >= treshold && this.Array[a]["accY"] <= treshold) {
+                            this.step++;
+                        }
+                    }
+                }
+            }
+            if ( this.result.Z == AxeMax ) {
+                let treshold = ((this.minZ + this.maxZ) / 2);
+                for (let i = 0; i < this.Array.length; i++){
+                    let a = i + 1;
+                    if (i !== (this.Array.length - 1) && (treshold > 0.5 || treshold < -0.5)) {
+                        if (this.Array[i]["accZ"] >= treshold && this.Array[a]["accZ"] <= treshold) {
+                            this.step++;
+                        }
+                    }
+                }
+            }
+
             this.api.post(apiUrl + '/add', JSON.stringify(this.Array), this.httpOptions).subscribe();
             this.Array.splice(0, 100);
+            this.result.X = 0;
+            this.result.Y = 0;
+            this.result.Z = 0;
+            this.maxX = 0;
+            this.minX = 0;
+            this.maxY = 0;
+            this.minY = 0;
+            this.maxZ = 0;
+            this.minZ = 0;
         }, 1000);
     }
 
